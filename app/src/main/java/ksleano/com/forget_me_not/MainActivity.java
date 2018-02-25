ef47d4b18698a7aa5f490652e6ac5f6d999cc304
ksleano <ksleano@gmail.com>
2018-02-24 18:12:13 -0800

    First Commit

---
+++ app/src/main/java/ksleano/com/forget_me_not/MainActivity.java
@@ -0,0 +1,13 @@
+package ksleano.com.forget_me_not;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
